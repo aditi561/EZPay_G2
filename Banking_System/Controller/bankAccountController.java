41855f4f02dae6811a57e277d7a02a521f09d7ef
aditi561 <53342413+aditi561@users.noreply.github.com>
2025-07-29 14:50:14 +0530

    bankAccountController.java

--- Banking_System/Controller/bankAccountController.java
+++ Banking_System/Controller/bankAccountController.java
@@ -6,25 +6,55 @@ import com.ezpay.bank.service.bankAccountServiceImpl;
 
 import java.util.List;
 
+/**
+ * Controller class for managing bank account operations.
+ */
 public class bankAccountController {
-    private bankAccountService bankAccountService = new bankAccountServiceImpl();
 
+    private final bankAccountService bankAccountService = new bankAccountServiceImpl();
+
+    /**
+     * Adds a new bank account.
+     *
+     * @param account The BankAccount object to be added.
+     */
     public void addAccount(BankAccount account) {
         bankAccountService.addAccount(account);
     }
 
+    /**
+     * Retrieves a bank account using the account number.
+     *
+     * @param accountNumber The account number.
+     * @return The BankAccount object if found, else null.
+     */
     public BankAccount getAccount(String accountNumber) {
         return bankAccountService.getAccountByNumber(accountNumber);
     }
 
+    /**
+     * Retrieves all bank accounts.
+     *
+     * @return List of all BankAccount objects.
+     */
     public List<BankAccount> getAllAccounts() {
         return bankAccountService.getAllAccounts();
     }
 
+    /**
+     * Updates the details of an existing bank account.
+     *
+     * @param account The BankAccount object with updated details.
+     */
     public void updateAccount(BankAccount account) {
         bankAccountService.updateAccount(account);
     }
 
+    /**
+     * Deletes a bank account by account number.
+     *
+     * @param accountNumber The account number of the bank account to delete.
+     */
     public void deleteAccount(String accountNumber) {
         bankAccountService.deleteAccount(accountNumber);
     }
