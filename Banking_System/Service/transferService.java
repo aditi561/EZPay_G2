0b85f6f0bda5e297e10e4443dc21d2ef80820a23
aditi561 <53342413+aditi561@users.noreply.github.com>
2025-07-29 14:55:41 +0530

    transferService.java

--- Banking_System/Service/transferService.java
+++ Banking_System/Service/transferService.java
@@ -3,8 +3,30 @@ package com.ezpay.bank.service;
 import com.ezpay.bank.model.Transfer;
 import java.util.List;
 
+/**
+ * Service interface for handling transfer-related operations.
+ */
 public interface transferService {
-    public void makeTransfer(Transfer transfer);
-    public Transfer getTransferById(int transferId);
-    public List<Transfer> getAllTransfers();
+
+    /**
+     * Records a new transfer.
+     *
+     * @param transfer The Transfer object to be recorded.
+     */
+    void makeTransfer(Transfer transfer);
+
+    /**
+     * Retrieves a transfer by its unique ID.
+     *
+     * @param transferId The ID of the transfer to retrieve.
+     * @return The Transfer object if found, otherwise null.
+     */
+    Transfer getTransferById(int transferId);
+
+    /**
+     * Retrieves all recorded transfers.
+     *
+     * @return A list of all Transfer objects.
+     */
+    List<Transfer> getAllTransfers();
 }
