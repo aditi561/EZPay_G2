0655de5b7cefe4a0957aab5ead80a9eb6332cb5d
aditi561 <53342413+aditi561@users.noreply.github.com>
2025-07-29 14:55:59 +0530

    transferServiceImpl.java

--- Banking_System/Service/transferServiceImpl.java
+++ Banking_System/Service/transferServiceImpl.java
@@ -6,20 +6,39 @@ import com.ezpay.bank.model.Transfer;
 
 import java.util.List;
 
+/**
+ * Implementation of the TransferService interface for managing transfers.
+ */
 public class transferServiceImpl implements transferService {
 
-    private TransferDao transferDao = new TransferDaoImpl();
+    private final TransferDao transferDao = new TransferDaoImpl();
 
+    /**
+     * Records a new transfer by delegating to the DAO.
+     *
+     * @param transfer The Transfer object to be recorded.
+     */
     @Override
     public void makeTransfer(Transfer transfer) {
         transferDao.recordTransfer(transfer);
     }
 
+    /**
+     * Retrieves a transfer by its ID.
+     *
+     * @param transferId The ID of the transfer.
+     * @return The Transfer object, or null if not found.
+     */
     @Override
     public Transfer getTransferById(int transferId) {
         return transferDao.getTransferById(transferId);
     }
 
+    /**
+     * Retrieves a list of all transfers.
+     *
+     * @return A list of Transfer objects.
+     */
     @Override
     public List<Transfer> getAllTransfers() {
         return transferDao.getAllTransfers();
