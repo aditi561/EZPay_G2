88f296f6042f00050b3191d4a3077c8f18f947c8
Simran Choudhary <108448487+simran21205@users.noreply.github.com>
2025-08-07 09:40:25 +0530

    Add files via upload

---
+++ UI/src/app/welcome/welcome.ts
@@ -0,0 +1,9 @@
+// src/app/welcome.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.html',
+  styleUrls: ['./welcome.css']
+})
+export class WelcomeComponent {}
